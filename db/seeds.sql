e00abb57d5fe542b60c08ec8f9615abf2840f7e7
Brett Fuller <bfuller123@gmail.com>
2017-10-25 21:14:51 -0500

    init

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+use burgers_db;
+
+INSERT INTO burgers
+VALUES ('cheeseburger', 0)
+,('bacon burger', 0)
+,('mushroom swiss', 0)
---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mo = require('method-override');
+const bp = require('body-parser');
+const path = require('path');
+const app = express();
